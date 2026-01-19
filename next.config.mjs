1050cfed87cf0271c215673ecda8380194433eca
Irina Ichim <onlyirina7@gmail.com>
2026-01-19 15:33:11 +0100

    fix: add @ alias for webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'via.placeholder.com'],
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(process.cwd(), 'src');
+    return config;
   },
 };
 
